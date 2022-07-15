e0a4b0504c87fa7f713058fb18157b2f33cd6ffa
Alana Alexander <laninsky@gmail.com>
2022-07-15 18:33:04 +1200

    Create sMLH_mtDNA.R

---
+++ sMLH/sMLH_mtDNA.R
@@ -0,0 +1,40 @@
+# This code compares sMLH by sampling location and mitochondrial haplotype
+
+# 1. Loading necessary libraries
+library(tidyverse)
+library(rstatix)
+
+# 2. Setwd
+setwd("Dropbox (Otago University)/PossumGenomeProject/PossumMitochondria/mtDNA_phylogenetic_analysis/")
+
+# 3. Reading in data (tab delimited)
+temp <- read_tsv("Codex_Possum_30Jun2022_mtDNA.txt")
+temp <- temp %>% select(sMLH,raxml_run1,Location,broad_location)
+
+# 4. Filtering to just Otago and Australia
+aus_otago <- temp %>% filter(broad_location %in% c("Otago","Australia"))
+
+ggplot(aus_otago) + 
+  geom_boxplot(mapping=aes(x=broad_location, y=sMLH,fill=raxml_run1)) +
+  geom_jitter(mapping=aes(x=broad_location, y=sMLH,fill=raxml_run1),shape=21, position=position_jitter(0.2),size=1) +
+  facet_wrap(~raxml_run1,nrow=1) +
+  scale_fill_manual(values=c("black","#0067ff", "#009000","#ff0000")) +
+  xlab("Location") +
+  theme_bw(base_size = 8) +
+  theme(legend.position="none",panel.border=element_rect(fill = NA)) +  
+  theme(axis.title=element_text(size=14,face="bold")) +
+  theme(
+    strip.background = element_blank(),
+    strip.text.x = element_blank()
+  )
+
+ggsave(filename="sMLH_mtDNA.pdf",plot = last_plot(),width=4,height=2,units="in")
+
+#5. T-test within haplotypes and across all samples
+t_test(data = aus_otago, 
+       formula = sMLH ~ broad_location, 
+       alternative = "two.sided")
+
+t_test(data = aus_otago %>% filter(raxml_run1=="Blue (NSW)"), 
+       formula = sMLH ~ broad_location, 
+       alternative = "two.sided")
