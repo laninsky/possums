3524c0ef738470b87eb1f1e9c86a1a09ae917bad
Alana Alexander <laninsky@gmail.com>
2023-04-06 21:43:56 +1200

    Update allele_specific.sh

--- allelic_specific/allele_specific.sh
+++ allelic_specific/allele_specific.sh
@@ -5,7 +5,10 @@ echo "chromosome: $3"
 echo "gene starting position: $4"
 echo "gene ending position: $5"
 
-
+echo "Subsetting gVCF file"
+grep "^\#" "$1" >> $3.$1
+grep -v "^\#" "$1" | grep "$3" >> $3.$1
 
 echo "Subsetting RNA VCF file"
+grep "^\#" "$2" >> $3.$2
 grep -v "^\#" "$2" | grep "$3" >> $3.$2
