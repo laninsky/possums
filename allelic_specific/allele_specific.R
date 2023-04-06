4595a3c9892b11a380aa3919e1b839d5d16bc2bc
Alana Alexander <laninsky@gmail.com>
2023-04-06 23:03:39 +1200

    Create allele_specific.R

---
+++ allelic_specific/allele_specific.R
@@ -0,0 +1,11 @@
+#!/usr/bin/env Rscript
+
+args = commandArgs(trailingOnly=TRUE)
+
+gVCF_file <- args[1]
+RNA_VCF_file <- args[2]
+chromosome <- args[3]
+start_pos <- args[4]
+end_pos <- args[5]
+
+write.table("testing",args[1])
